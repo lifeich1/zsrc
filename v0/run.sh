d84e8d06ae33d03c56e7fedf8e07ec4910623330
lifeich0 <lifeich0@gmail.com>
2018-08-18 11:01:44 +0800

    :hankey: Add bad net speeder
    * 卯用没有的双发包加速工具

--- v0/run.sh
+++ v0/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+while getopts "s" arg
+do
+    case $arg in
+        s)
+            ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
+            if [ "$ETH"  ==  '1' ] ; then
+                nohup /usr/local/bin/net_speeder eth0 "ip" >>/root/nohup.out 2>&1 &
+            fi
+            if [ "$ETH"  ==  '0' ] ; then
+                nohup /usr/local/bin/net_speeder venet0 "ip" >>/root/nohup.out 2>&1 &
+            fi
+            echo 'net_speeder start'
+            break
+    esac
+done
+
 
 if [[ -e $SSR_CONFPTH ]]; then
     python server.py -c $SSR_CONFPTH
