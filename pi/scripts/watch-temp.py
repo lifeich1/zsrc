8527dc74eaf3aa1600d106cd8235d530ec517138
lifeich1 <lifeich0@gmail.com>
2019-09-05 18:16:13 +0800

    :construction: refactor to oo

--- pi/scripts/watch-temp.py
+++ pi/scripts/watch-temp.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser(description='CPU temperature watcher')
+parser.add_argument('-hi', '--temp-hi', type=float, required=True,
+                    help='enable threshold')
+parser.add_argument('-lo', '--temp-lo', type=float, required=True,
+                    help='disable threshold')
+parser.add_argument('-n', '--nsec-wait', type=int, default=5,
+                    help='time wait between detections')
+parser.add_argument('--log-file', default='/var/log/z/watch-temp.log')
+parser.add_argument('--log-report-time-count', type=int, default=24)
+
+args = parser.parse_args()
+
 import time
 import sys
 
 TEMP_FILE = '/sys/class/thermal/thermal_zone0/temp'
-LOG_LV_HI = 50
-LOG_LV_LO = 46
 FREQ_FILE = '/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq'
-PERIOD = 5
-LOG_FILE = '/var/log/z/watch-temp.log'
 
 def get_temp():
     with open(TEMP_FILE, 'r') as f:
@@ -26,24 +36,29 @@ def fmt_out(T, Fs, vt=False):
     return '%11d%7.1f%6.3f%6.3f%6.3f%6.3f' % (int(time.time()) if vt else 0, T, *Fs)
 
 def printl(s):
-    with open(LOG_FILE, 'a') as f:
+    with open(args.log_file, 'a') as f:
         print(s, file=f, flush=True)
 
 
 last_hi = False
-print('t_hi', LOG_LV_HI, 't_lo', LOG_LV_LO, 'log:', LOG_FILE, file=sys.stderr, flush=True)
+log_hi_cnt = 0
+print('t_hi', args.temp_hi, 't_lo', args.temp_lo, 'log:', args.log_file, file=sys.stderr, flush=True)
 
 while True:
     t = get_temp()
-    if ((not last_hi) and t > LOG_LV_HI) or (last_hi and t > LOG_LV_LO):
+    if ((not last_hi) and t > args.temp_hi) or (last_hi and t > args.temp_lo):
         fs = get_cpu_freqs()
-        printl(fmt_out(t, fs, not last_hi))
+        printl((' ' if last_hi else '+') + fmt_out(t, fs, (not last_hi) or log_hi_cnt > args.log_report_time_count))
         last_hi = True
+        if log_hi_cnt > args.log_report_time_count:
+            log_hi_cnt = 0
+        log_hi_cnt += 1
     elif last_hi:
         # exit rec
         last_hi = False
+        log_hi_cnt = 0
         fs = get_cpu_freqs()
         printl(fmt_out(t, fs, vt=True))
 
-    time.sleep(PERIOD)
+    time.sleep(args.nsec_wait)
 
