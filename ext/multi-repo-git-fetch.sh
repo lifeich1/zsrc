fa9966a25772187c3e18e357b97887cb3fd2eda6
lifeich1 <lifeich0@gmail.com>
2019-11-06 14:51:03 +0800

    :wrench: fetch tool

---
+++ ext/multi-repo-git-fetch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+usage() {
+    cat << END
+usage: $0 [-i <input-file>]
+description: read git repos path and do fetch
+
+-i <input-file>     use <input-file> to replace stdin
+END
+    exit 2
+}
+
+frm_stdin=1
+
+args=`getopt hi: $*`
+
+for i in $args; do
+    case $i in
+        -i)
+            frm_stdin=0; input="$2"; shift;
+            shift;;
+        -h)
+            usage;;
+    esac
+done
+
+do_fetch() {
+    if [ -e "$1" ]; then
+        echo "[$1]"
+        git -C $1 fetch
+    else
+        echo "[Warn]: '$1' not exists"
+    fi
+}
+
+do_loop() {
+    while read wdir; do
+        do_fetch $wdir
+    done
+}
+
+if [ $frm_stdin -ne 0 ]; then
+    do_loop
+else
+    cat $input | do_loop
+fi
