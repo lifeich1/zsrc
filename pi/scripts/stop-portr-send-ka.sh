4af017c05532209e052c3ccef6c351d2591f71b5
lifeich1 <lifeich0@gmail.com>
2019-11-15 11:07:55 +0800

    :pencil2: fix typo

--- pi/scripts/stop-portr-send-ka.sh
+++ pi/scripts/stop-portr-send-ka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo `date` >> /var/tmp/tmprun.log
+#echo `date` >> /var/tmp/tmprun.log
 
 /usr/bin/python3 -m portr_act -s `cat /home/pi/Code/etc/portr/ka-secret` \
     cli -u `cat /home/pi/Code/etc/portr/ka-url` -o b
